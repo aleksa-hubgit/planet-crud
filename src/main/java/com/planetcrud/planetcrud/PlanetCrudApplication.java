1d22d8ab9785b1269c0d9e31c9bd806c621811b1
aleksa-hubgit <aleksa.stev92@gmail.com>
2023-03-09 20:09:34 +0100

    Spring boot project initialization.

---
+++ src/main/java/com/planetcrud/planetcrud/PlanetCrudApplication.java
@@ -0,0 +1,13 @@
+package com.planetcrud.planetcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planetcrud/planetcrud/PlanetCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetcrud.planetcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
